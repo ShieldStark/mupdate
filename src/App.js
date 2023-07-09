de911779da81cd0138d048b2e15e3435f3b41cb9
Piyush <piyushabhiranjan30@gmail.com>
2023-07-09 14:28:12 +0530

    FlexBox

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Main from './Components/Home/main'
 import HoverEnlarge from './Components/Hover/HoverEnlarge';
 import HoverSlider from './Components/Hover/HoverSlider';
 import Sidebar from './Components/SideBar/sidebar';
+import ProgessBar from './Components/progress/progressBar'
 function App() {
   return (
     <div>
+
       {/* <h1>Hover to Enlarge</h1>
        <HoverSlider />
       <HoverEnlarge /> */}
-      <Sidebar/>
+      {/* <Sidebar/> */}
+      <ProgessBar/>
     </div>
   );
 }
---
+++ src/Components/progress/progressBar.js
@@ -0,0 +1,14 @@
+import './progressBar.css'
+const progressBar = ()=>{
+    return(
+        <div className='box-container'>
+        <p>Project Progress</p>
+        <div className="progress-container">
+            <div className="progress-bar" ></div>
+        </div>
+        </div>
+
+    );
+};
+export default progressBar;
+
